ab8139337bc92e8fd1a1dfc17a80f08587f8ef3d
Bhakti Sawarkar <bhakti123.java@gmail.com>
2018-08-21 20:27:56 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -2,6 +2,29 @@
 
 $(document).ready(function(){
 
-// call functions here
+function getIt(){
+  $('p').on('click', function(){
+    alert ('Hey!')
+  });
+}
 
+function frameIt(){
+  $(document).on('load', function{
+    img.classList.add("tasty");
+  });
+}
+
+function pressIt(){
+  $(document).on('keydown', function(){
+    if(key.which == 70){
+      alert("G was pressed");
+    }
+  });
+}
+
+function submitIt(){
+  $("form").on("submit", function(){
+    alert('your form is submitted now');
+  });
+}
 });
